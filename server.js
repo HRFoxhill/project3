8ff44d242b2f9e410cbe7e7d15df7fdf69284caa
Amanda Malmin <amandamalmin@gmail.com>
2018-08-18 18:19:29 -0500

    updated package.json to correctly run server.js (still a bug w/ router.use)

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
+// Dependencies
+// ===================================
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+// const path = require("path");
 const routes = require("./routes");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+var logger = require("morgan");
+
+// Initialize Express
+// =====================================
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
+// Configure middleware
+// =====================================
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+
+// !!anyone know if we need both of these?
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to the Mongo DB
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/artdb";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Routes
+// =====================================
 // Add routes, both API and view
 app.use(routes);
 
@@ -22,9 +44,6 @@ app.use(routes);
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/artdb");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);

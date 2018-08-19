a58f463adcff2d84652c0047025beea7df4d438b
jon Opacich <jonopacich@gmail.com>
2018-08-19 16:38:39 -0500

    added routing in app.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ class Home extends Component {
   render() {
     return (
         <div>
-            Hello WOrld!
+          Hello World!
         </div>
     )
   }

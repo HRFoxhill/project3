f69e60773431fc948d12a9def1331b848b6d0f63
Artaviia <jonopacich@gmail.com>
2018-08-18 20:31:39 -0500

    update

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+class Home extends Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+        <div>
+            Hello WOrld!
+        </div>
+    )
+  }
+}
+
+export default Home;

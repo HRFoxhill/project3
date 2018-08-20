454f59de589088b37451cb1057e33e29d756d69d
Amanda Malmin <amandamalmin@gmail.com>
2018-08-19 22:16:04 -0500

    added bulma & bulma-start to package.json - may need to add it to the client one too?

--- client/src/components/SignUpModal/SignUpModal.js
+++ client/src/components/SignUpModal/SignUpModal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SignUpModal.css";
+import 'bulma/css/bulma.css'
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually

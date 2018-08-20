96decd56c9e77f34284a605147f7a2ae13c33709
HaleyB <haleyb128@gmail.com>
2018-08-20 10:33:29 -0500

    added files for components footer, navbar, artistPanel, artworkPanel, localShows

---
+++ client/src/pages/Artist_Portfolio/Artist_Portfolio.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+//nav
+//artist panel & info
+    //profile image
+    //name, statement, website url, categories, phone #, 
+//shows
+    //name, location, dates
+//artwork panels
+    //artwork image
+    //title, year. dimensions, category, description, SOLD(?)
+//footer
+
+
+
+
+
+export default Artist_Portfolio;

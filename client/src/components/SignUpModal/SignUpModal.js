a25acbebc14fc003af7100188e36db4620fb5532
Amanda Malmin <amandamalmin@gmail.com>
2018-08-19 21:28:41 -0500

    created component folder, basic outline signIn/Up components

---
+++ client/src/components/SignUpModal/SignUpModal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SignUpModal.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+const SignUpModal = props => (
+  ddd
+);
+
+export default SignUpModal;

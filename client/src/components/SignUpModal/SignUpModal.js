9f63a6a52e6e24ef12a5636b34b433ca98aa7d2a
Amanda Malmin <amandamalmin@gmail.com>
2018-08-20 08:27:42 -0500

    committing before git pull

--- client/src/components/SignUpModal/SignUpModal.js
+++ client/src/components/SignUpModal/SignUpModal.js
@@ -5,66 +5,82 @@ import SignInBtn from "../SignInBtn";
 import PasswordInputBox from "../PasswordInputBox";
 import PasswordInputBox from "../UsernameInputBox";
 
-// The ...props means, spread all of the passed props onto this element
-// That way we don't have to define them all individually
-const Modal = props => (
-    <div class="modal">
-        <div class="modal-background"></div>
-        <div class="modal-content">
-            {/* email */}
-            <div class="field">
-                <label class="label">Email</label>
-                <div class="control has-icons-left has-icons-right">
-                    <input class="input is-danger" type="email" placeholder="Email input" value="hello@" />
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-envelope"></i>
-                    </span>
-                    <span class="icon is-small is-right">
-                        <i class="fas fa-exclamation-triangle"></i>
-                    </span>
-                </div>
-                <p class="help is-danger">This email is invalid</p>
-            </div>
-            
-            <UsernameInputBox />
-            <PasswordInputBox />
+class SignUpModal extends Component {
 
-            {/* Terms & conditions */}
-            <div class="field">
-                <div class="control">
-                    <label class="checkbox">
-                        <input type="checkbox" />
+    render() {
+        <div class="modal">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                {/* email */}
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-danger" type="email" placeholder="Email input" value="hello@" />
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                            <i class="fas fa-exclamation-triangle"></i>
+                        </span>
+                    </div>
+                    <p class="help is-danger">This email is invalid</p>
+                </div>
+
+                <UsernameInputBox />
+                <PasswordInputBox />
+
+                {/* confirm password box */}
+                <div class="field">
+                    <label class="label">Confirm</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-success" type="password" placeholder="*******" value="bulma" {...props} />
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                            <i class="fas fa-check"></i>
+                        </span>
+                    </div>
+                    <p class="help is-success">It's a match!</p>
+                </div>
+
+                {/* Terms & conditions */}
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                            <input type="checkbox" />
                             I agree to the <a href="#">terms and conditions</a>
-                    </label>
+                        </label>
+                    </div>
                 </div>
-            </div>
 
-            <div class="field">
-                <div class="control">
-                    <label class="radio">
-                        <input type="radio" name="question" />
-                        Yes
+                <div class="field">
+                    <div class="control">
+                        <label class="radio">
+                            <input type="radio" name="question" />
+                            Yes
     </label>
-                    <label class="radio">
-                        <input type="radio" name="question" />
-                        No
+                        <label class="radio">
+                            <input type="radio" name="question" />
+                            No
     </label>
+                    </div>
                 </div>
-            </div>
 
-            {/* Submit/close btns */}
-            <div class="field is-grouped">
-                <div class="control">
-                    <button class="button is-link">Submit</button>
-                </div>
-                <div class="control">
-                    <button class="button is-text">Cancel</button>
+                {/* Submit/close btns */}
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-link">Submit</button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-text">Cancel</button>
+                    </div>
                 </div>
             </div>
+            <button class="modal-close is-large" aria-label="close"></button>
+            <p>Already have an account? </p> <SignInBtn />
         </div>
-        <button class="modal-close is-large" aria-label="close"></button>
-        <p>Already have an account? </p> <SignInBtn />
-    </div>
-);
+    }
+};
 
-export default Modal;
+export default SignUpModal;

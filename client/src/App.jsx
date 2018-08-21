e6ce6f12f06480ac6c034d649404fff0b0cac9fd
jon Opacich <jonopacich@gmail.com>
2018-08-20 22:57:39 -0500

    added footer + navbar, minor app.jsx changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
+import Nav from "./components/navbar";
+import Footer from "./components/footer";
+import DevCard from "./components/DevCards";
 
 const App = () => (
   <Router>
     <div>
+      <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route component={NoMatch} />
       </Switch>
+      <Footer />
     </div>
   </Router>
 );

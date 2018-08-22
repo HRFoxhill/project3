44290d1b0c43c0d5c349aa4ab6ea8314ed3e257f
HaleyB <haleyb128@gmail.com>
2018-08-21 21:18:23 -0500

    added files and pseudo code for editArt and editInfo

---
+++ client/src/components/featured/featured.jsx
@@ -0,0 +1,8 @@
+//featured hero container
+<section class="hero is-primary is-medium">
+  <div class="hero-body">
+    <div class="container">
+    {props.children}
+    </div>
+  </div>
+</section>

69db1529240774640d27893dbf7b18ac91263aaa
HaleyB <haleyb128@gmail.com>
2018-08-24 09:21:14 -0500

    minor fixes

--- client/src/components/artistPanel/artistPanel.jsx
+++ client/src/components/artistPanel/artistPanel.jsx
@@ -3,31 +3,37 @@ import React from "react";
 
 
 const artistPanel = props => (
-    <section className="hero is-medium is-primary is-bold">
+    <section className="hero is-medium is-info is-bold">
         <div className="hero-body">
             <div className="container">
                 <div className="columns">
-                    <div className="column is-one-third">
+                    <div className="column is-centered">
                         <figure className="image is-128x128">
                             <img className="is-rounded" src={props.profilePhoto}/>
                         </figure>
                     </div>
-                    <div class="column is-two-thirds">
-                        <h1 class="title">
-                            {props.artistName}
-                        </h1>
-                        <h2 class="subtitle">
-                            {props.bio}
-                        </h2>
-                        <h2 class="subtitle">
-                            {props.websiteURL}
-                        </h2>
-                        <h2 class="subtitle">
-                            {props.phone}
-                        </h2>
-                        <h2 class="subtitle">
-                            {props.categories}
-                        </h2>
+                    <div class="column is-three-quarters is-centered">
+                        <div className="tile is-ancestor">
+                            <div className="tile is-parent">
+                                <article class="tile is-child notification is-primary">
+                                    <h1 class="title">
+                                        {props.artistName}
+                                    </h1>
+                                    <h2 class="subtitle">
+                                        {props.bio}
+                                    </h2>
+                                    <h2 class="subtitle">
+                                        {props.websiteURL}
+                                    </h2>
+                                    <h2 class="subtitle">
+                                        {props.phone}
+                                    </h2>
+                                    <h2 class="subtitle">
+                                        {props.categories}
+                                    </h2>
+                                </article>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/pages/Artist_Portfolio/Artist_Portfolio.jsx
+++ client/src/pages/Artist_Portfolio/Artist_Portfolio.jsx
@@ -9,23 +9,41 @@ import APIArt from "../../utils/APIArt"
 class portfolio extends Component{
     state = {
         Artist: {
-            profilePhoto: "",
-            artistName: "",
-            bio: "",
-            websiteURL: "",
-            phone: "",
-            mediums: ""
+            email: "DBrewer@Art.com",
+            password: "password",
+            artistName: "Dick Brewer",
+            phone: "506-344-3232",
+            websiteURL: "http://dixplexia.com/",
+            bio: "My work revolves around vibration and the transformation of energy. I use a 21,000 RPM die grinder to mine abstract images. Over the course of 27 years, the vibration from the die grinder has shaken me out of the mundane and into the sacred. As I work, I listen to music and let it guide my movements. Music and sound are the underpinnings of my work. The marks I make reference techno, jazz, symphonic and primitive forms. The whining of the grinder abrading the plex mingles with the baseline and melody, punctuated with my own percussion. The effect is mesmerizing and meditative. A rhythmic, harmonic hum creates a gateway to higher consciousness through a vibrating field of visual sound, blurring sensory boundaries.",
+            profilePhoto: "http://dixplexia.com/thumbnails/pics/17Isisframe.jpg"
         },
-        Art: []
+        Art: [
+            {
+                title: "Isis",
+                medium: "Mixed Media",
+                url: "http://dixplexia.com/thumbnails/pics/17Isisframe.jpg",
+                dimensions: "27x21",
+                yearCreated: "2017",
+                description: "Created using a 21,000 ROM grinder.",
+            },
+            {
+                title: "Eldorado Blvd",
+                medium: "Mixed Media",
+                url: "http://dixplexia.com/thumbnails/pics/5EldoradoBlvdframe.jpg",
+                dimensions: "29x23",
+                yearCreated: "2018",
+                description: "Created using a 21,000 ROM grinder.",
+            },
+        ]
     };
-    componentDidMount = event => {
+    // componentDidMount = event => {
 
-        this.setState({
-            Artist: APIArtists.getArtistByName("Dick Brewer"),
-            Art: APIArt.getAllArt()
-          })
+    //     this.setState({
+    //         Artist: APIArtists.getArtistByName("Dick Brewer"),
+    //         Art: APIArt.getAllArt()
+    //       })
           
-      }
+    //   }
 
     render() {
         return (

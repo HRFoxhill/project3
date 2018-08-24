78f060ebd50b4ae06b1a606c5ba3862c7b5b6a8e
Amanda Malmin <amandamalmin@gmail.com>
2018-08-24 11:31:05 -0500

    moved medium api call to function, calling it onMount and onUpdate

--- client/src/pages/Medium/Medium.jsx
+++ client/src/pages/Medium/Medium.jsx
@@ -13,43 +13,44 @@ class Medium extends Component {
         profilePhoto: "",
         artistName: "",
     };
-    
-    componentDidUpdate = event => {
+    handleMediumSearch = () => {
+        let parsedUrlMedium = window.location.href.split("=").pop();
+        let populatedArtArray = [];
+
+        this.setState({
+            medium: parsedUrlMedium,
+        })
+        console.log(parsedUrlMedium)
+
+        // get art by medium
+        APIArt.getArtByMedium(parsedUrlMedium)
+            .then(data => {
+                // console.log(data.data);
+                data.data.forEach(item => {
+                    APIArtists.getArtistByArt(item._id)
+                        .then(results => {
+                            item.artistInfo = results.data[0]
+                            populatedArtArray.push(item);
+                            
+                        })
+                        .catch(err => console.log(err));
+                })
+                this.setState({
+                    art: populatedArtArray
+                })
+                console.log(this.state.art)
+            })
+            .catch(err => console.log(err));
+    }
+    componentDidMount = () => {
+        this.handleMediumSearch();
+    };
+
+    componentDidUpdate = () => {
         let parsedUrlMedium = window.location.href.split("=").pop();
 
         if (parsedUrlMedium !== this.state.medium) {
-            let populatedArtArray = [];
-
-            this.setState({
-                medium: parsedUrlMedium,
-            })
-            console.log(parsedUrlMedium)
-
-            // get art by medium
-            APIArt.getArtByMedium(parsedUrlMedium)
-                .then(data => {
-                    console.log(data.data);
-                    data.data.forEach(item => {
-                        APIArtists.getArtistByArt(item._id)
-                            .then(results => {
-                                // bundle all that info into a useful state to be rendered on that page
-                                
-                                item.artistInfo = results.data[0]
-                                console.log(results.data[0]);
-                                console.log(item)
-                                populatedArtArray.push(item);
-                                this.setState({
-                                    art:  populatedArtArray
-                                })
-                                console.log(this.state.art)
-                            })
-                            .catch(err => console.log(err));
-                    })
-
-                    
-                    // console.log(this.state.art)
-                })
-                .catch(err => console.log(err));
+            this.handleMediumSearch();
         };
     };
 
@@ -57,18 +58,7 @@ class Medium extends Component {
         return (
             <div>
                 hello world
-                {/* the below coding is breaking - not sure why but took it out for now just for testing purposes - Amanda*/}
-                {/* <Discover>
-                  {this.state.art.map(artist => {
-                      return (
-                        <ArtistCard
-                          featuredPhoto={featuredPhoto}
-                          profilePhoto={profilePhoto}
-                          artistName={artistName}
-                        />
-                      )
-                  })}  
-                </Discover> */}
+
             </div>
         );
     }

a02348ebb1e78aa08bc2ccc56ce2cbdcdb7925bd
notoriousp <pegmickelson+school@gmail.com>
2018-08-24 14:16:02 -0500

    Added medium.jsx and mediumPanel.jsx

--- client/src/pages/Medium/Medium.jsx
+++ client/src/pages/Medium/Medium.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import APIArt from "../../utils/APIArt";
 import APIArtists from "../../utils/APIArtists";
-import Discover from "../../components/discover";
+import MediumPanel from "../../components/mediumPanel";
 import ArtistCard from "../../components/artistCard";
+import {ArtworkContainer} from "../../components/artworkContainer";
+
+// import mediumPanel from "../../components/artistPanel/mediumPanel";
 // import localShows from "../../components/localShows";
 
 class Medium extends Component {
     state = {
         medium: "",
         art: [],
-        featuredPhoto: "",
-        profilePhoto: "",
-        artistName: "",
+        
+        
+        // featuredPhoto: "",
+        // profilePhoto: "",
+        // artistName: "",
     };
     handleMediumSearch = () => {
         let parsedUrlMedium = window.location.href.split("=").pop();
@@ -31,7 +36,7 @@ class Medium extends Component {
                         .then(results => {
                             item.artistInfo = results.data[0]
                             populatedArtArray.push(item);
-                            
+
                         })
                         .catch(err => console.log(err));
                 })
@@ -54,14 +59,38 @@ class Medium extends Component {
         };
     };
 
+    consoleLog = (data)=> {
+        console.log(data)
+    }
+    
     render() {
         return (
             <div>
-                hello world
-
+                <ArtworkContainer>
+                    {this.state.art.map(art => {
+                        return (
+                            <MediumPanel
+                            
+                                artTitle={art.title}
+                                artMedium={art.medium}
+                                artURL={art.url}
+                                artDimensions={art.dimensions}
+                                artYearCreated={art.yearCreated}
+                                artDescription={art.description}
+                                artArtistName={art.artistInfo.artistName}
+                                artArtistWebsite={art.artistInfo.artistWebsite}
+                            />
+                        );
+                    })}
+                </ArtworkContainer>
             </div>
-        );
+        )
     }
 }
+
+
+
+
+
 
 export default Medium;

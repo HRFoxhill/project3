1a506cd02ed21bc4d8c2365f75be26d918a2bbb0
HaleyB <haleyb128@gmail.com>
2018-08-25 14:11:18 -0500

    working on styling the portfolio page

--- client/src/components/artistPanel/artistPanel.jsx
+++ client/src/components/artistPanel/artistPanel.jsx
@@ -6,33 +6,32 @@ const artistPanel = props => (
     <section className="hero is-medium is-primary is-bold" id="PanelWrapper">
         <div className="hero-body">
             <div className="container">
-                <div className="columns">
-                    <div className="column is-centered">
+                <div className="level">
+                    <div className="level-left">
                         <figure className="image is-2x3">
                             <img className="is-rounded" id="artistPanelImage" src={props.profilePhoto}/>
                         </figure>
                     </div>
-                    <div className="column is-three-quarters is-centered" id="artistPanelContainer">
-                        <div className="tile is-ancestor">
-                            <div className="tile is-parent" id="artistPanelBox">
-                                <article className="tile is-child notification is-primary">
-                                    <h1 className="title" id="artistPanelTitle">
+                    <div className="level-right">
+                        <div class="container" id="artist-info-container">
+                                    <p className="title is-3 is-center" id="artistPanelTitle">
                                         {props.artistName}
-                                    </h1>
-                                    <h2 className="subtitle" id="artistPanelSubtitle">
-                                        {props.bio}
-                                    </h2>
-                                    <h2 className="subtitle" id="artistPanelSubtitle">
+                                    </p>
+                                    <p className="subtitle is-5 artist-panel-text">
                                         {props.websiteURL}
-                                    </h2>
-                                    <h2 className="subtitle" id="artistPanelSubtitle">
+                                    </p>
+                                    <p className="subtitle is-5 artist-panel-text">
                                         {props.phone}
-                                    </h2>
-                                    <h2 className="subtitle" id="artistPanelSubtitle">
+                                    </p>
+                                    <p className="subtitle is-5 artist-panel-text">
                                         {props.categories}
-                                    </h2>
-                                </article>
-                            </div>
+                                    </p>
+                                    <div>
+                                        <p className="subtitle is-5 artist-panel-bio">
+                                            {props.bio}
+                                        </p>
+                                    </div>
+                            
                         </div>
                     </div>
                 </div>
@@ -42,3 +41,40 @@ const artistPanel = props => (
 );
 
 export default artistPanel;
+
+{/* <section className="hero is-medium is-primary is-bold" id="PanelWrapper">
+<div className="hero-body">
+    <div className="container">
+        <div className="columns">
+            <div className="column is-centered">
+                <figure className="image is-2x3">
+                    <img className="is-rounded" id="artistPanelImage" src={props.profilePhoto}/>
+                </figure>
+            </div>
+            <div className="column is-three-quarters is-centered" id="artistPanelContainer">
+                <div className="tile is-ancestor">
+                    <div className="tile is-parent" id="artistPanelBox">
+                        <article className="tile is-child notification is-primary">
+                            <h1 className="title" id="artistPanelTitle">
+                                {props.artistName}
+                            </h1>
+                            <h2 className="subtitle" id="artistPanelSubtitle">
+                                {props.bio}
+                            </h2>
+                            <h2 className="subtitle" id="artistPanelSubtitle">
+                                {props.websiteURL}
+                            </h2>
+                            <h2 className="subtitle" id="artistPanelSubtitle">
+                                {props.phone}
+                            </h2>
+                            <h2 className="subtitle" id="artistPanelSubtitle">
+                                {props.categories}
+                            </h2>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section> */}

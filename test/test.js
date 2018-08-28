9c5c55a8a51bfe85a7511783a67e8d6e30fd7600
notoriousp <pegmickelson+school@gmail.com>
2018-08-28 07:17:49 -0500

    Updated test.js file

--- test/test.js
+++ test/test.js
@@ -7,14 +7,14 @@ describe('ArtSpose', function () {
   this.timeout('20s')
 
   Nightmare.action('waitforunload', function (done) {
-    this.evaluate_now(function() {
-        return new Promise(function(resolve, reject) {
-            window.onbeforeunload = function() {
-                resolve();
-            };
-        });
+    this.evaluate_now(function () {
+      return new Promise(function (resolve, reject) {
+        window.onbeforeunload = function () {
+          resolve();
+        };
+      });
     }, done)
-}); 
+  });
 
   let nightmare = null
   beforeEach(() => {
@@ -22,65 +22,70 @@ describe('ArtSpose', function () {
   })
 
 
-    // it('should successfully arrive at the home page', done => {
-    //   // your actual testing urls will likely be `http://localhost:port/path`
-    //   nightmare.goto('http://localhost:3000/')
-    //     .end()
-    //     .then(function (result) { done() })
-    //     .catch(done)
-    // })
-
-    
-    // it('should successfully arrive at the Paintings medium page', done => {
-    //   // your actual testing urls will likely be `http://localhost:port/path`
-    //   nightmare.goto('http://localhost:3000/medium=Painting')
-    //   .wait(1500)
-    //     .end()
-    //     .then(function (result) { done() })
-    //     .catch(done)
-    // })
+  it('should successfully arrive at the home page', done => {
+    // your actual testing urls will likely be `http://localhost:port/path`
+    nightmare.goto('http://localhost:3000/')
+      .end()
+      .then(function (result) { done() })
+      .catch(done)
+  })
 
 
-    // it('should successfully search on medium', done => {
-    //   new Nightmare({ show: true })
-    //     .goto('http://localhost:3000/')
-    //     .type(".input", "ceramics")
-    //     .click(".is-info")
-    //     .evaluate(function () {
-    //       return document.querySelector(".")
-    //       //. would be class selector; # would be ID name; anchor or a div would be an element - use []'s
-    //     })
-    //     .then(function (result) {  
-    //     expect(result) .to .not .equal(undefined);
-    //     //expect(result) .to .equal(localhost:3000/?search?ceramics) if I were using window.location on line 38; line 39 return would be ...
-    //     done()  
-    //     })
-    //     .catch(done)
-    // })
+  it('should successfully arrive at the Paintings medium page', done => {
+    // your actual testing urls will likely be `http://localhost:port/path`
+    nightmare.goto('http://localhost:3000/medium=Painting')
+      .wait(1500)
+      .end()
+      .then(function (result) { done() })
+      .catch(done)
+  })
 
-    it('should indicate invalid email', function(done)  {
-      new Nightmare({ show: true})
+
+  // it('should successfully search on medium', done => {
+  //   new Nightmare({ show: true })
+  //     .goto('http://localhost:3000/')
+  //     .type(".input", "ceramics")
+  //     .click(".is-info")
+  //     .evaluate(function () {
+  //       return document.querySelector(".")
+  //       //. would be class selector; # would be ID name; anchor or a div would be an element - use []'s
+  //     })
+  //     .then(function (result) {  
+  //     expect(result) .to .not .equal(undefined);
+  //     //expect(result) .to .equal(localhost:3000/?search?ceramics) if I were using window.location on line 38; line 39 return would be ...
+  //     done()  
+  //     })
+  //     .catch(done)
+  // })
+
+  it('login', function (done) {
+    new Nightmare({ show: true })
       .goto('http://localhost:3000/')
       .click("#signInModalOpen")
       .wait("#signInEmailInputBox")
-            .type("#signInEmailInputBox", "heatheriscool")
-            .type("#signInPasswordInputBox", "pAssword")
-      // .waitforunload()
-     .click("#signInBtn")
-    .evaluate(function () {
-    $(document).find ("#signInBtn").click()
-      console.log("Thanks Joe",document.querySelector(".help"));
-      
-      return document.querySelector(".help")[0].innerText
-    })
-    // .end()
-    .then(function (result) {
-      expect(result).to.equal("incorrect email or password");
-      done()
-    })
-   
-  
-    })
+      .type("#signInEmailInputBox", "pegmickelson@gmail.com")
+      .type("#signInPasswordInputBox", "12345678")
+      .end()
+      .then(function (result) { done() })
+      .catch(done)
+  })
+
+  // .waitforunload()
+  //  .click("#signInBtn")
+  // .evaluate(function () {
+  // $(document).find ("#signInBtn").click()
+  //   console.log("Thanks Joe",document.querySelector(".help"));
+
+  //   return document.querySelector(".help")[0].innerText
+  // })
+  // .end()
+  // .then(function (result) {
+  //   expect(result).to.equal("incorrect email or password");
+  //   done()
+  // })
+
+
+})
     // it('should bring up login modal', done => {
     //   new Nightmare({show: true})
     //   .goto('http://localhost:3000/'),
@@ -91,4 +96,4 @@ describe('ArtSpose', function () {
 
     //   })
     // })
-  })
+  // })
